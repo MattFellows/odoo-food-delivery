84a9f0cc93ef0357e4348b50444238c45e6643ca
Matt <gitkraken@matt-fellows.me.uk>
2018-12-08 09:09:01 +0000

    fix: fix address on order out for delivery

--- src/components/OutForDeliveryOrders/OutForDeliveryOrders.js
+++ src/components/OutForDeliveryOrders/OutForDeliveryOrders.js
@@ -37,7 +37,7 @@ class OutForDeliveryOrders extends OrderComponent {
                             Items: {o.orderLines ? o.orderLines.map((ol) => {
                             return (<p key={ol.id}>{ol.name}</p>);
                         }) : ""}
-                            <div>Address: {this.props.order.partnerDetails && this.props.order.partnerDetails.length > 0 ? this.props.order.partnerDetails[0].contact_address.split('\n').map((line) => {
+                            <div>Address: {o.partnerDetails && o.partnerDetails.length > 0 ? o.partnerDetails[0].contact_address.split('\n').map((line) => {
                                 return (<p key={line} className={"reduceHeight"}>{line}</p>);
                             }) : ""} </div>
                         </div>);
